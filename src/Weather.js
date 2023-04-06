3ca797d7b9ad148997d28c7d536832b2b4fdb0d3
ChiaoH <118980040+ChiaoH@users.noreply.github.com>
2023-04-06 16:46:22 +0200

    Deleted weather app to retry.

---
+++ src/Weather.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+export default function Weather() {
+  return <h2>Hello</h2>;
+}
